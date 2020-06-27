9810867f3128a87fc8fc50a3f00a4d9e94ffcbff
Rafael Schmid <rafaelfschmid@gmail.com>
2020-06-27 13:46:05 -0400

    config file .py

---
+++ scripts/config.py
@@ -0,0 +1,17 @@
+# Abbreviations of each strategy, used in csv and scurve
+abbreviations = {'bbsegsort':'H','mergeseg':'M','radixseg':'R','nthrust':'MT','fixthrust':'FT','fixcub':'FC'}
+
+# Symbols to be plotted in scurve
+symbols = {'bbsegsort':'.-','mergeseg':'*-','radixseg':'v-','nthrust':'x-','fixthrust':'m+-','fixcub':'y|-'}
+
+# Colors of each strategy
+colors = {'bbsegsort':'green','mergeseg':'blue','radixseg':'red','nthrust':'purple','fixthrust':'brown','fixcub':'orange'}
+
+# Gerar o csv? True or False
+texGenerator = False
+
+# Gerar o csv? True or False
+csvGenerator = True
+
+# Gerar o csv? True or False
+scurveGenerator = True
